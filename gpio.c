1dbbd02af0da2f73587e66656bfaed38496ef62d
computermouth <computermouth@crunchbangplusplus.org>
2017-06-26 19:13:13 -0700

    initial commit

---
+++ gpio.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#define CHIPPRO_GPIO_ROOT /sys/class/gpio
+#define CHIPPRO_GPIO_P_D0 gpio132
+#define CHIPPRO_GPIO_DIRE direction
+#define CHIPPRO_GPIO_VALU value
+
+int main(void){
+  
+  
+  printf("hello\n");
+  return 0;
+}
